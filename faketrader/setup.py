97126e81a06ed0e4e33239440431396727e40ffe
anmirjan <anishmirjankar@gmail.com>
2019-02-21 13:31:17 -0500

    mlp structure/definition

---
+++ faketrader/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='faketrader',
+      version='0.1',
+      description='Fake trader tools to initialize and demo application',
+      url='https://github.com/amirjankar/vector-field-neural-network',
+      author='Anish Mirjankar',
+      author_email='anishmirjankar@gmail.com',
+      license='MIT',
+      packages=['faketrader'],
+      zip_safe=False)
